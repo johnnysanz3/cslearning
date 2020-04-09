01eb48cb852cc0f3908003dc007fcf961e31c3f4
John Santillo-Herbert <johnsantilloherbert@gmail.com>
2020-04-09 16:11:34 -0400

    changes to generic exercises

--- src/main/java/com/johnnysanz/javatutorial/generics/Exercises.java
+++ src/main/java/com/johnnysanz/javatutorial/generics/Exercises.java
@@ -5,20 +5,28 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Exercises {
 
     public static void main(String[] args){
 
         //test List with generic method
-        List<Integer> testListOfIntegers = Arrays.asList(1,2,3,4,5,6,7,8,10,12,14,0,0);
+        List<Integer> testList = Arrays.asList(1,2,3);
         Exercises testEx = new Exercises();
         //passes in predicate lambda to check for odds
-        System.out.println("Int is " + testEx.countOdds(testListOfIntegers, p -> {return p % 2 != 0;}));
+        System.out.println("Result using defined method is " + testEx.countIfMatches(testList, p -> {return p % 2 != 0;}));
+
+        // can i rewrite this using streams
+        long count = testList.stream()
+                .filter(p -> {return p % 2 != 0;})
+                .count();
+
+        System.out.println("Result using streams is " + count);
 
     }
 
-    private <T> int countOdds(Collection<T> inCollection, Predicate<T> p){
+    private <T> int countIfMatches(Collection<T> inCollection, Predicate<T> p){
 
         //return size of collection as test
         //return inCollection.size();
@@ -30,10 +38,8 @@ public class Exercises {
             }
         }
         return count;
-
-
-
-
     }
+
+
 
 }
